5076d99167ac07d416896cbda2d84e98814ae516
Neil <nmcconlogue@apa.org>
2023-07-03 10:50:40 -0400

    initial commit from start.spring.io

---
+++ src/main/java/net/mcfarb/springroll/SpringRollApplication.java
@@ -0,0 +1,13 @@
+package net.mcfarb.springroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRollApplication.class, args);
+	}
+
+}
